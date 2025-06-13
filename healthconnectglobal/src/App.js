0471295fb1b30336c98e9f1027b9416b9efbc9da
DesireMusarafu <desiremusarafu36@gmail.com>
2025-06-14 02:13:40 +0530

    1

--- healthconnectglobal/src/App.js
+++ healthconnectglobal/src/App.js
@@ -1,9 +1,11 @@
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -13,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learning React
+          Learn React
         </a>
       </header>
     </div>
